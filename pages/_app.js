5e0463cc65bdf35cc8c19dbd46690016f67d326f
Gustavo Saiani <gustavo@poe.ma>
2022-05-18 23:08:48 +0200

    Add Google Sign-in

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import '../styles/globals.css'
 
+const clientId = "452795776568-6n0he2jkh824kj8j8nvv74ia2dvvtue7.apps.googleusercontent.com";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <GoogleOAuthProvider clientId={clientId}>
+    <Component {...pageProps} />
+  </GoogleOAuthProvider>
 }
 
 export default MyApp
