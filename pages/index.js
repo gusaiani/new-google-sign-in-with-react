2ce45d0a0a9ea26c2eff51664d4ef9df653736ab
Gustavo Saiani <gustavo@poe.ma>
2022-05-19 14:47:19 +0200

    Add button with custom look

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => console.log(tokenResponse),
+  });
+
   return (
     <div className={styles.container}>
       <GoogleLogin
@@ -14,6 +18,10 @@ export default function Home() {
           console.log('Login Failed');
         }}
       />
+
+      <button onClick={() => login()}>
+        Sign in with Google 🚀{' '}
+      </button>
     </div>
   )
 }
